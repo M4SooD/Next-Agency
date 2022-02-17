b75d366243e7a630b1d66de811607cbbceb47dc2
masood <masooodm@yahoo.com>
2022-02-17 11:40:47 +0330

    commit

---
+++ components/Intro.jsx
@@ -0,0 +1,33 @@
+import styles from "../styles/Intro.module.css";
+import Image from "next/image";
+import Circle from "./Circle";
+const Intro = () => {
+  return (
+    <div className={styles.container}>
+      <Circle backgroundColor="#01c686" top="-45vh" left="-45vh" />
+      <Circle backgroundColor="#01c686" right="-40vh" />
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          <span className={styles.brand}>AV0CAD0</span>DIGITAL PRODUCT AGENCY
+        </h1>
+        <p className={styles.desc}>
+          Create live segments and target the right people for messages based on
+          their behaviors.
+        </p>
+        <button className={styles.button}>DISCOVER</button>
+      </div>
+      <div className={styles.card}>
+        <Image
+          src="/../public/img/Avocado.png"
+          width="100%"
+          height="100%"
+          layout="fill"
+          objectFit="cover"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
